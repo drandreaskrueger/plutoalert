d71ad647c36801d8e0cff0583ba48362a05d1f6d
drandreaskrueger <drandreaskrueger@users.noreply.github.com>
2022-02-20 21:55:43 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="drandreaskrueger"
+original_name="plutoalert"
+original_urlname="plutoalert"
+original_description="Awesome plutoalert created by drandreaskrueger"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv plutoalert $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
