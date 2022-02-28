6104ec22eb070360c863baba4d11b30991f6cb30
Andreas Krueger <coding@AndreasKrueger.de>
2022-03-01 00:17:55 +0100

    make fmt

--- plutoalert/__init__.py
+++ plutoalert/__init__.py
@@ -1,9 +1,5 @@
-from . import plutotv # noqa
+from . import plutotv  # noqa
 from .base import BaseClass, base_function
 from .plutotv import the_purpose_of_all_this_v2
 
-__all__ = [
-    "BaseClass",
-    "base_function",
-    "the_purpose_of_all_this_v2"
-]
+__all__ = ["BaseClass", "base_function", "the_purpose_of_all_this_v2"]
